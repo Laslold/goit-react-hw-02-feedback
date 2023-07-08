c4daec71aa90ef96566467e6726169fe11c9153f
Viacheslav Shylko <116965660+Laslold@users.noreply.github.com>
2023-07-08 19:27:08 +0300

    start

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { ButtonStyle } from "./Button.styled.js";
+const Button = ({ children, variant = "none", ...restProps }) => {
+  return (
+    <ButtonStyle {...restProps} className={variant}>
+      {children}
+    </ButtonStyle>
+  );
+};
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["none", "tertiary", "primary", "secondary"]),
+};
+
+export default Button;
